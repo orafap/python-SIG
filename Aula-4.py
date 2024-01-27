71c19bc6d6618021985cdceef95d33158d83cddd
Rafael Pereira <157925150+orafap@users.noreply.github.com>
2024-01-27 20:36:36 -0300

    Add files via upload

---
+++ Aula-4.py
@@ -0,0 +1,45 @@
+# Operadores, Condicionais e loops
+
+# Operadores:
+# São operadores ferramentas usadas na construção de expressões na liguagem python.
+# Os operadores são divididos em: aritméticos, lógicos, comparação e condicionais.
+
+# Operadores Aritméticos:
+# São operadores responsáveis por operações matemáticas na variáveis em python.
+# Os operadores aritméticos são divididos em: adição, subtração, multiplicação, divisão, exponencial, modulo.
+
+# Operadores Aritméticos - Adição
+# Operadores de adição são responsáveis de somar variáveis do tipo numéricos e juntar variáveis como texto e na estrutura de lista,
+# são responsáveis por operações matemáticas na variáveis em python. 
+# Exemplo:
+a = 1
+b = 2
+c = b+a
+print (c)
+
+# Variáveis do tipo lista
+a = [1,2]
+b = [3,4]
+c = b + a
+print(c)
+ 
+# Variáveis do tipo string
+d = 'Python'
+e = 'SIG'
+f = d+e
+print(f)
+
+# Operadores Aritméticos - Subtração
+# Operadores de subtração são responsáveis de subtrair variáveis do tipo númericos.
+a = 1 
+b = 2
+c = b-a
+print(c)
+
+# Operadores Aritméticos - Multiplicação
+# Operadores de multiplicação são responsáveis em multiplicar variáveis do tipo númerico e repetir variáveis string
+c = a*b
+print(c)
+c = e
+f = e*3
+print(f)
